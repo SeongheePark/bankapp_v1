963c73b85f369bc21114d806ebdc97a64c165dda
SeongheePark <sungheepppp@gmail.com>
2023-04-17 12:31:15 +0900

    기본 화면 구성 1

--- src/main/java/com/tenco/bank/repository/interfaces/HistoryRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/HistoryRepository.java
@@ -1,5 +1,16 @@
 package com.tenco.bank.repository.interfaces;
 
-public interface HistoryRepository {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tenco.bank.repository.model.History;
+
+@Mapper
+public interface HistoryRepository {
+	public int insert(History history);
+	public int updateById(History history);
+	public int deleteById(int id);
+	public List<History> findAll();
+	public History findById(int id);
 }
--- src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/UserRepository.java
@@ -12,6 +12,6 @@ public interface UserRepository {
 	public int insert(User user);
 	public int updateById(User user);
 	public int deleteById(Integer id);
-	public User findById(Integer id);
 	public List<User> findAll();
+	public User findById(Integer id);
 }
