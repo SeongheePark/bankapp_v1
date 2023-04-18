e9fa0f7999d9ae62dc42880401c45dae925b7cbf
SeongheePark <sungheepppp@gmail.com>
2023-04-18 12:35:36 +0900

    회원가입 트랜잭션 처리까지

--- src/main/java/com/tenco/bank/controller/UserController.java
+++ src/main/java/com/tenco/bank/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.tenco.bank.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,20 +9,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tenco.bank.dto.SignInFormDto;
 import com.tenco.bank.dto.SignUpFormDto;
+import com.tenco.bank.handler.exception.CustomRestfullException;
+import com.tenco.bank.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
-
+	
+	@Autowired // DI 처리
+	private UserService userService;
+	
 	// http://localhost:8080/user/sign-up
 	@GetMapping("/sign-up")
 	public String signUp() {
 		// prefix, suffix
 		return "/user/signUp";
 	}
-	
+
 	/**
 	 * 회원가입 처리
+	 * 
 	 * @param signUpFormDto
 	 * @return 리다이렉트 로그인 페이지
 	 */
@@ -29,27 +37,42 @@ public class UserController {
 	// dto : object mapper 처리
 	@PostMapping("/sign-up")
 	public String signUpProc(SignUpFormDto signUpFormDto) {
+		// 1. 유효성 검사
+		if (signUpFormDto.getUsername() == null || signUpFormDto.getUsername().isEmpty()) {
+			throw new CustomRestfullException("username을 입력해주세요!", HttpStatus.BAD_REQUEST);
+		}
+		if (signUpFormDto.getPassword() == null || signUpFormDto.getPassword().isEmpty()) {
+			throw new CustomRestfullException("password를 입력해주세요!", HttpStatus.BAD_REQUEST);
+		}
+		if (signUpFormDto.getFullname() == null || signUpFormDto.getFullname().isEmpty()) {
+			throw new CustomRestfullException("fullname을 입력해주세요!", HttpStatus.BAD_REQUEST);
+		}
+		
+		// 2. 서비스 호출
+		userService.signUp(signUpFormDto);
+		
 		return "redirect:/user/sign-in";
 	}
-	
+
 	/**
 	 * 로그인 폼
+	 * 
 	 * @return 로그인 페이지 리턴
 	 */
 	@GetMapping("/sign-in")
 	public String signIn() {
 		return "/user/signIn";
 	}
-	
+
 	/**
 	 * 로그인 처리
+	 * 
 	 * @param signInFormDto
 	 * @return 메인 페이지 이동 (수정 예정)
-	 */ 
+	 */
 	@PostMapping("/sign-in")
 	public String signInProc(SignInFormDto signInFormDto) {
 		return "/test/main";
 	}
-	
 
 }
---
+++ src/main/java/com/tenco/bank/service/UserService.java
@@ -0,0 +1,29 @@
+package com.tenco.bank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tenco.bank.dto.SignUpFormDto;
+import com.tenco.bank.handler.exception.CustomRestfullException;
+import com.tenco.bank.repository.interfaces.UserRepository;
+
+@Service // IoC 대상
+public class UserService {
+	// DAO 만들지 않아도 된다.. 인터페이스 가져오기
+	@Autowired // DI 처리 (객체 생성 시 의존 주의 처리)
+	private UserRepository userRepository;
+	
+	@Transactional 
+	// 메서드 호출이 시작될 때 트랜잭션 시작
+	// 메서드 종료 시 트랜잭션 종료 (commit)
+	public void signUp(SignUpFormDto signUpFormDto) {
+		// DB 접근 처리
+		// User
+		int result = userRepository.insert(signUpFormDto);
+		if (result != 1) {
+			throw new CustomRestfullException("회원가입 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
